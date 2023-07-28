e881750969724ebf6d8646a2400053c9a0014b4a
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-28 01:38:28 -0300

    feature cetificados de qualidade e ajuste do titulo de seção

--- sections/Content/SectionTitle.tsx
+++ sections/Content/SectionTitle.tsx
@@ -9,7 +9,7 @@ function SectionTitle({ title}: Props) {
   const id = useId();
 
   return (
-    <div class={`relative z-[1]`}>
+    <div class={`relative z-[1] flex items-center justify-center`}>
       <h2 class={`section-title title-line`}>{title}</h2>
     </div>
   );
