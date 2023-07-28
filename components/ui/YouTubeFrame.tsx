cafec58807f87bbee6bbd46d08ea22199fda2dd5
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-28 02:25:47 -0300

    Feature VapzaWorks2

--- components/ui/YouTubeFrame.tsx
+++ components/ui/YouTubeFrame.tsx
@@ -1,20 +1,21 @@
 interface Props {
-    videoUrl?: string;
-    width?: number;
-    height?: number;
+  videoUrl?: string;
+  width?: number;
+  height?: number;
 }
 
 const YouTubePlayer = ({ videoUrl, width, height }: Props) => {
-    const embedUrl = `${videoUrl}`;
-  
-    return (
-      <iframe
-        width={width}
-        height={height}
-        src={embedUrl}
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      ></iframe>
-    );
-  };
-  
-  export default YouTubePlayer;
+  const embedUrl = `${videoUrl}`;
+
+  return (
+    <iframe
+      width={width}
+      height={height}
+      src={embedUrl}
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    >
+    </iframe>
+  );
+};
+
+export default YouTubePlayer;
--- sections/Content/VapzaWorks.tsx
+++ sections/Content/VapzaWorks.tsx
@@ -1,7 +1,7 @@
 import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
 import { useId } from "preact/hooks";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
-import YouTubeFrame from "$store/components/ui/YouTubeFrame.tsx"
+import YouTubeFrame from "$store/components/ui/YouTubeFrame.tsx";
 
 export interface Props {
   iframe: {
@@ -9,39 +9,45 @@ export interface Props {
     width?: number;
     height?: number;
     backgroundIframe?: LiveImage;
-  }
+  };
   title: string;
   description: string;
   banner: LiveImage;
   alt?: string;
 }
 
-function VapzaWorks({ title, description, banner, iframe, alt}: Props) {
+function VapzaWorks({ title, description, banner, iframe, alt }: Props) {
   const id = useId();
 
   return (
     <>
-        <div class={`py-[30px] bg-base-200 bg-[url('${iframe.backgroundIframe}')]`}>
-            <YouTubeFrame videoUrl={iframe.videoUrl} width={iframe.width} height={iframe.height}/>
-        </div>
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-        </div>
-        <div class="flex items-center justify-center px-4">
-            <Picture>
-                <Source
-                src={banner}
-                width={345}
-                height={345}
-                />
-                <img
-                class="w-full h-auto mb-4"
-                src={banner}
-                alt={alt}
-                />
-            </Picture>
-        </div>
+      <div
+        class={`py-[30px] bg-base-200 bg-[url('${iframe.backgroundIframe}')]`}
+      >
+        <YouTubeFrame
+          videoUrl={iframe.videoUrl}
+          width={iframe.width}
+          height={iframe.height}
+        />
+      </div>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div class="flex items-center justify-center px-4">
+        <Picture>
+          <Source
+            src={banner}
+            width={345}
+            height={345}
+          />
+          <img
+            class="w-full h-auto mb-4"
+            src={banner}
+            alt={alt}
+          />
+        </Picture>
+      </div>
     </>
   );
 }
