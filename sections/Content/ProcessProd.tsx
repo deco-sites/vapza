46bfa1337f67498d5874481e51ddd908122c324e
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-27 23:20:39 -0300

    formatação

--- sections/Content/ProcessProd.tsx
+++ sections/Content/ProcessProd.tsx
@@ -20,7 +20,7 @@ export interface Props {
   cards?: Card[];
 }
 
-function CardItem({ card }: { card: Card}) {
+function CardItem({ card }: { card: Card }) {
   const {
     image,
     alt,
@@ -31,49 +31,50 @@ function CardItem({ card }: { card: Card}) {
 
   return (
     <>
-    <div class="flex flex-row items-center justify-start w-full odd:flex-row-reverse">
-      <div>
-        <Picture>
-          <Source
-            src={image}
-            width={112}
-          />
-          <img
-            class="h-auto w-[112px] min-w-[112px]"
-            src={image}
-            alt={alt}
-          />
-        </Picture>
-      </div>
-      <div>
-        <h3 class={`uppercase text-[13px] leading-[18px] mx-5 tracking-wider text-primary`}>
-          {title}
-        </h3>
-        {description &&
-          <p class={`text-xs mx-5 tracking-widest`}>{description}</p>}
-      </div>
-    </div>
-    {arrowImage &&
-      (
-      <>
-      <div></div>
-      <div>
-        <Picture >
-          <Source
-            src={arrowImage}
-            width={150}
-          />
-          <img
-            class="w-full h-auto max-w-[150px] my-5"
-            src={arrowImage}
-          />
-        </Picture>
+      <div class="flex flex-row items-center justify-start w-full odd:flex-row-reverse">
+        <div>
+          <Picture>
+            <Source
+              src={image}
+              width={112}
+            />
+            <img
+              class="h-auto w-[112px] min-w-[112px]"
+              src={image}
+              alt={alt}
+            />
+          </Picture>
         </div>
-        </>
-      )}
+        <div>
+          <h3
+            class={`uppercase text-[13px] leading-[18px] mx-5 tracking-wider text-primary`}
+          >
+            {title}
+          </h3>
+          {description &&
+            <p class={`text-xs mx-5 tracking-widest`}>{description}</p>}
+        </div>
+      </div>
+      {arrowImage &&
+        (
+          <>
+            <div></div>
+            <div>
+              <Picture>
+                <Source
+                  src={arrowImage}
+                  width={150}
+                />
+                <img
+                  class="w-full h-auto max-w-[150px] my-5"
+                  src={arrowImage}
+                />
+              </Picture>
+            </div>
+          </>
+        )}
       {!arrowImage &&
-      (<div></div>
-      )}
+        <div></div>}
     </>
   );
 }
@@ -87,8 +88,12 @@ function ProcessProd({ title, cards }: Props) {
         id={id}
         class="relative flex flex-col items-center justify-center px-4 z-[1]"
       >
-        <h2 class={`title-line uppercase text-xl py-2.5 px-4 mb-4 mx-[50px] text-primary text-center leading-6 bg-base-200`}>{title}</h2>
-        {cards?.map((image, index) => <CardItem card={image} key={index}/>)}
+        <h2
+          class={`title-line uppercase text-xl py-2.5 px-4 mb-4 mx-[50px] text-primary text-center leading-6 bg-base-200`}
+        >
+          {title}
+        </h2>
+        {cards?.map((image, index) => <CardItem card={image} key={index} />)}
       </div>
     </div>
   );
