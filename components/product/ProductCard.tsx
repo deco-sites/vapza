a58dc1223cc74c3db4cd127af9682c042441d567
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-08-02 00:50:25 -0300

    fix: nome do rpoduto nos cards de prateleira

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -167,7 +167,12 @@ function ProductCard({ product, preload, itemListName, layout }: Props) {
                 ? ""
                 : (
                   <h2 class="uppercase text-xs lg:text-lg text-black">
-                    {name}
+                    {isVariantOf?.name != name ? (
+                      <>
+                        {isVariantOf?.name}
+                      </>
+                    )
+                    : <>{name}</>}
                   </h2>
                 )}
             </div>
